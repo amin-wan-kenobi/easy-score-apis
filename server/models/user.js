b3c6b1decce807462ab57f450f94d07c69159ac9
Amin <ameri.env@gmail.com>
2017-07-25 22:33:26 -0400

    Register API is done here

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,62 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
-    //Define the schema of your document
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    surname:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    token: {
+        type: String,
+        trim: true
+    },
+    mobile:{
+        type: String,
+        trim: true
+    },
+    accountNo:{
+        type: String,
+        trim: true
+    }
 });
 
-//Define both Model and Instance methods of this model here
+UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'name', 'surname', 'userName', 'mobile']);
+}
+
+UserSchema.methods.generateToken = function(){
+    var user = this;
+    var access = 'login_access';
+    var token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, process.env.JWT_SECRET).toString();
+
+    user.token = token;
+    return user.save().then((user) => {
+        return token;
+    });
+}
 
 
 //Define your database event schema here: i.e. pre. before saving the details into the db
--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
 require('./config/config');
- 
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-var {mongoose} = require('./db/mongoose.js');
-const {ObjectID} = require('mongodb');
- 
+var { mongoose } = require('./db/mongoose.js');
+const { ObjectID } = require('mongodb');
+const { User } = require('./models/user');
+
 var app = express();
 const port = process.env.PORT;
- 
+
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-  
-app.get('/', (req, res) => {
-    res.send('Happy Coding!!!');
+
+app.post('/register', (req, res) => {
+    let body = _.pick(req.body, ['username', 'password', 'name', 'surname', 'mobile', 'accountNo']);
+    let user = new User(body);
+    user.save().then((user) => {
+        return user.generateToken();
+    }).then((token) => {
+        res.send({user, token});
+    }).catch((e) => res.status(400).send(e));
 });
- 
- 
+
+
 app.listen(port, () => console.log(`Server started and listening to Port ${port}`));
- 
- 
-module.exports = {app};
+
+
+module.exports = { app };
